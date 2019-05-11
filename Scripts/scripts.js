183b768530b0871f2238f37cba3b020bf2bfab9f
knyg <kevin1992yang@gmail.com>
2019-05-11 03:04:06 -0700

    initial main func

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
     convertToSpongeBobCase = () => {
         let user_input = document.getElementsByClassName('user-input')[0].value.toLowerCase();
 
-        console.log(user_input);
+        for (let i = 0; i < user_input.length; i += 2) {
+            console.log(user_input[i])
+        }
     }
 
 });
