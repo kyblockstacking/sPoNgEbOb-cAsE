b4e7aa3075406f34bce8de884c091629a9955531
knyg <kevin1992yang@gmail.com>
2019-05-11 03:22:07 -0700

    some func logic

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -2,10 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
     convertToSpongeBobCase = () => {
         let user_input = document.getElementsByClassName('user-input')[0].value.toLowerCase();
+        let results = '';
 
-        for (let i = 0; i < user_input.length; i += 2) {
-            console.log(user_input[i])
+        for (let i = 1; i < user_input.length; i += 2) {
+            // skip space characters for spongebob case
+            if (user_input[i] === ' ') {
+                continue;
+            }
+            // console.log(user_input[i - 1].toLowerCase())
+            // console.log(user_input[i].toUpperCase())
+
+            results += user_input[i - 1].toLowerCase();
+            results += user_input[i].toUpperCase();
         }
+
+        console.log(results);
     }
 
 });
